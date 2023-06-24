6a640e0a430143e4d01a1ebca23d97d1d0fc6044
Florian Körner <contact@florian-koerner.com>
2023-06-24 09:01:50 +0200

    position

--- src/main/java/fr/epicanard/globalmarketchest/managers/GroupLevels.java
+++ src/main/java/fr/epicanard/globalmarketchest/managers/GroupLevels.java
@@ -98,9 +98,7 @@ public enum GroupLevels {
         builder.addField("itemMeta");
         builder.addField("COUNT(itemMeta) AS count");
 
-        Collections.reverse(Arrays.asList(items));
-
-        builder.setExtension("GROUP BY itemMeta ORDER BY FIELD(name,\"" + String.join("\",\"", items) + "\") DESC, itemMeta ASC");
+        builder.setExtension("GROUP BY itemMeta ORDER BY position ASC, itemMeta ASC");
         break;
       case 1:
         builder.setExtension("ORDER BY price ASC, start ASC");
